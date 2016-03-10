b4cd2b04edc0badd875c1621a3e72e1aee1501f3
Krzysztof Burlinski <krzysztof.burlinski@gmail.com>
2016-03-10 14:21:30 +0100

    Adding angular-wesnoth-tiles.

---
+++ client/app/game/game.js
@@ -0,0 +1,12 @@
+'use strict';
+angular.module('mudServerApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('game', {
+        url: '/game',
+        templateUrl: 'app/game/game.html',
+        controller: 'GameController',
+        controllerAs: 'game',
+        authenticate: 'game'
+      });
+  });
