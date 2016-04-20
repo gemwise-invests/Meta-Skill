73f18345f045aae795e87514c53800633b265751
syzer3@gmail.com <syzer3@gmail.com>
2016-04-20 18:32:12 +0200

    viking directive

--- client/app/character/viking.card.directive.js
+++ client/app/character/viking.card.directive.js
@@ -3,26 +3,20 @@ angular.module('mudServerApp')
         return {
             restrict: 'EA',
             scope: {
-                model: "=",
-                onHexClicked: "&",
-                showCursor: "&",
-                scrollable: "&",
-                onPreDraw: "&",
-                onPostDraw: "&"
+                pickAvatar: "="
             },
             template: `
             <div class="col-sm-6 col-md-4">
                 <div class="thumbnail hover-grow">
-                    <a class="thumbnail" ng-click="$ctrl.pickAvatar('viking')">
+                    <a class="thumbnail" ng-click="pickAvatar('viking')">
                         <img alt="techno viking" class="avatar-card"
                              src="assets/avatars/techno-viking-sticker.png">
                     </a>
                     <div class="caption">
                         <h3>Viking</h3>
                         <p><strong>About:</strong> Vikings were the Justin Biebers of the 10th century.
-                            They have the power of <strong><a
-                                href="https://youtu.be/FwsntHcWiy4?t=63">techno</a></strong>
-                            and <strong>hammertime</strong>.
+                            They have the power of <strong><a href="https://youtu.be/FwsntHcWiy4?t=63">techno</a>
+                            </strong> and <strong>hammertime</strong>.
                             They also are fans of strong type systems without type inference.
                         </p>
                     </div>
