48981f622cf24dc91ea16d0ef36129923b29c5c1
Krzysztof Burlinski <krzysztof.burlinski@gmail.com>
2016-04-24 22:16:54 +0200

    New release of Angular-Wesnoth-Tiles.

--- client/app/game/_game.js
+++ client/app/game/_game.js
@@ -1,7 +1,10 @@
 'use strict';
 
 angular.module('mudServerApp.game', [
-    'mudServerApp.auth',
-    'ui.router',
-    'WesnothTiles'
-]);
+        'mudServerApp.auth',
+        'ui.router',
+        'WesnothTiles'
+    ])
+    .constant("WesnothTilesConfig", {
+        path: "bower_components/wesnoth-tiles/tiles/"
+    })
